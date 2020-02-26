737f370bacc8fe9185ce6f8d06f3ea8f703dc2e6
Travis Oliphant <teoliphant@gmail.com>
2020-02-26 12:49:29 -0600

    Clean up package for inital registering

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup
 
 setup(
+    version='0.0.1',
     name='epython',
+    url='https://github.com/epython-dev/epython',
+    description='A typed subset of Python to be used as an extension language',
+ ssss='epython',
+    author='Quansight Labs',
+    author_email = 'labs@quansight.com',
+    license='BSD-3',
+    packages=['epython'],
     entry_points={
         'console_scripts': [
-            'epython = epython:main',
+            'epython = epython.epython:main',
         ],
-    }
+    },
+   zip_safe=False,
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
